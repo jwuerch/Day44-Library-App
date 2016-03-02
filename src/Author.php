bdc0024d53d647570aff9c54cc02a6d8b1de8e60
Jason Wuerch <wuerchjason@gmail.com>
2016-03-02 09:18:04 -0800

    pass spec for getting first name of author

---
+++ src/Author.php
@@ -0,0 +1,29 @@
+<?php
+
+    class Author {
+        private $first_name;
+        private $last_name;
+        private $id;
+
+        public function __construct($first_name, $last_name, $id = null) {
+            $this->first_name = $first_name;
+            $this->last_name = $last_name;
+            $this->id = $id;
+        }
+
+        //Setters;
+        public function setFirstName($new_first_name) {
+            $this->first_name = $new_first_name;
+        }
+        public function setLastName($new_last_name) {
+            $this->last_name = $new_last_name;
+        }
+
+        //Getters;
+        public function getFirstName() {
+            return $this->first_name;
+        }
+    }
+
+
+ ?>
---
+++ tests/AuthorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Author.php";
+
+    $server = 'mysql:host=localhost;dbname=library_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class AuthorTest extends PHPUnit_Framework_TestCase {
+
+        // protected function teardown() {
+        //
+        // }
+
+        function testGetFirstName() {
+            //Arrange;
+            $first_name = 'Daniel';
+            $last_name = 'Quinn';
+            $test_author = new Author($first_name, $last_name);
+
+            //Act;
+            $result = $test_author->getFirstName();
+
+            //Assert;
+            $this->assertEquals($first_name, $result);
+        }
+    }
+
+
+ ?>
